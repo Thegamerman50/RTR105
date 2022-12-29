5dccfdc167f9f8c5fce696c59a0e675f33f272a8
Thegamerman50 <113163079+Thegamerman50@users.noreply.github.com>
2022-12-29 14:13:07 +0200

    Update while.c

--- Programmesanas_uzd/while.c
+++ Programmesanas_uzd/while.c
@@ -1,1 +1,94 @@
+#include <stdio.h>
+#include <math.h>
 
+ char wa = 0, dt1;
+ int dt2, iz;
+ long long dt3, st, sk, fa=1;
+ unsigned long long tests;
+
+
+ int main()
+ {
+
+ printf("Lūdzu iezvēlieties datu tipu:\n char = 1  int = 2  long long = 3\n");
+ scanf("%d",&iz);
+
+ printf("Lūdzu ievadi skaitli:    ");
+ scanf("%lld",&sk);
+
+     if(iz == 1)
+     {
+         dt1 = sk;
+         tests = dt1;
+         while(sk - fa > 1)
+         {
+             st = sk - fa;
+             tests = tests * st;
+             dt1 = dt1 * st;
+
+            fa++;
+
+            if(tests == dt1);
+            
+            else 
+            {
+            printf("Ir pārsniegts char limits");
+                break;
+            }
+         }
+
+        if (tests == dt1) printf("%lld! = %d", sk, dt1);
+     }
+    
+    
+
+     else if(iz == 2)
+     {
+         dt2 = sk;
+         tests = sk;
+         while(sk - fa > 1)
+         {
+             st = sk - fa;
+             tests = tests * st;
+             dt2 = dt2 * st;
+            fa++;
+
+            if(tests == dt2);
+            
+            else 
+            {
+            printf("Ir pārsniegts int limits");
+                break;
+            }
+         }
+
+        if (tests == dt2) printf("%lld! = %d", sk, dt2);
+     }
+
+    else 
+     {
+         dt3 = sk;
+         tests = sk;
+         while(sk - fa > 1)
+         {
+             st = sk - fa;
+             tests = tests * st;
+             dt3 = dt3 * st;
+         
+            fa++;
+          
+             if(dt3 < -1)
+            
+            {
+                wa = 1;
+            printf("Ir pārsniegts long long limits");
+               break;
+            }
+
+         }
+
+         if (wa == 0)printf("%lld! = %lld", sk, dt3);
+     }
+
+     return 0;
+ }
